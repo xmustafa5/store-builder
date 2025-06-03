466b7a6c080ebbe8767f2ee50cdcb19889e71a66
xmustafa5 <musmoh73@gmail.com>
2025-06-03 13:37:59 +0300

    Enhance IconsModal component with improved styling, layout adjustments, and increased accessibility features for icon selection.

--- app/standerd-layer/components/Icons.tsx
+++ app/standerd-layer/components/Icons.tsx
@@ -31,34 +31,36 @@ export default function IconsModal({
   return (
     <div
       onClick={() => setIsEditIcon(false)}
-      className="fixed inset-0 bg-black/50 backdrop-blur-[1px] flex items-center justify-center"
+      className="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50"
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="bg-white rounded-lg p-4 shadow-lg w-96"
+        className="bg-white rounded-xl p-6 shadow-2xl w-[948px] max-h-[90vh]"
       >
         <input
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search icons..."
-          className="w-full p-2 border border-gray-300 rounded-md mb-2"
+          className="w-full p-3 border border-gray-200 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
-        <div className="h-[446px] overflow-y-auto">
+        <div className="max-h-[calc(90vh-140px)] overflow-y-auto pr-2">
           {Object.entries(filteredIcons).map(([groupName, icons]) => (
-            <div key={groupName} className="mb-6">
-              <h3 className="text-lg font-semibold mb-3 text-gray-700">
+            <div key={groupName} className="mb-8 last:mb-2">
+              <h3 className="text-lg font-semibold mb-4 text-gray-800">
                 {groupName}
               </h3>
-              <div className="grid grid-cols-2 gap-4">
+              <div className="grid grid-cols-4 gap-3">
                 {icons?.map((icon: string) => (
                   <div
                     key={icon}
                     onClick={() => setItemIcon(icon)}
-                    className="p-2 hover:bg-gray-100 rounded cursor-pointer gap-2 flex flex-col items-center justify-center w-full"
+                    className="p-3 hover:bg-gray-50 transition-colors duration-200 rounded-lg cursor-pointer gap-2.5 flex flex-col items-center justify-center w-full border border-gray-100 hover:border-gray-200"
                   >
-                    <i className={`ri-${icon} text-2xl`}></i>
-                    <p>{icon?.split("/")[1]}</p>
+                    <i className={`ri-${icon} text-2xl text-gray-700`}></i>
+                    <p className="text-sm text-gray-600">
+                      {icon?.split("/")[1]}
+                    </p>
                   </div>
                 ))}
               </div>
